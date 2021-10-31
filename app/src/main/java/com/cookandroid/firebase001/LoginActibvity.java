3643f9c70945ed6f6546349f4c5b517425c2958d
SangjinHan1998 <xmfl34@naver.com>
2021-10-31 23:26:41 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/firebase001/LoginActibvity.java
@@ -0,0 +1,76 @@
+package com.cookandroid.firebase001;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActibvity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatebaseRef; // 실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd;      // 로그인 입력필드
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_actibvity);
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatebaseRef = FirebaseDatabase.getInstance().getReference("Firebase001");
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pw);
+
+
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그인 요청
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActibvity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())    {
+                            // 로그인 성공
+                            Intent intent = new Intent(LoginActibvity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();   // 현재 액티비티 파괴
+                        }  else {
+                            Toast.makeText(LoginActibvity.this, "로그인에 실패하셨습니다. ", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 회원가입 화면 이동
+                Intent intent = new Intent(LoginActibvity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
